57ed2f41741b48d91c555ea46a8b682db6363765
richardisaninja2 <95777257+richardisaninja2@users.noreply.github.com>
2024-10-18 12:28:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Items from './components/items/Items';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Recipe from './components/recipe/Recipe';
+import { useState } from 'react';
 
 function App() {
+
+  const [recipe, setRecipe] = useState();
+  const[recipeLong, setRecipeLong] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Items setRecipe={setRecipe}/>}/>
+        <Route path="/:title" element={<Recipe setRecipeLong={setRecipeLong} recipe={recipe && recipe}/>}/>
+      </Routes>
+    </BrowserRouter>
+    
+    
+      
     </div>
   );
 }
